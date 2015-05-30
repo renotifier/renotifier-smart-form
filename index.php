6d55c2ec945ba33d15716572dcc2013e7af35689
Janez K <janez.kranjc@gmail.com>
2015-05-30 14:34:00 +0200

    added utf8 metatag

--- index.php
+++ index.php
@@ -60,6 +60,7 @@
 <html>
     <head>
         <title>ReNotifier Demo - Smart Form</title>
+        <meta charset="UTF-8">        
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>   
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
--- thankyou.php
+++ thankyou.php
@@ -12,6 +12,7 @@
 <html>
     <head>
         <title>ReNotifier Demo - Standard Form</title>
+        <meta charset="UTF-8">        
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>   
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
